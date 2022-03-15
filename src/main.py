c1802b9681b2f69122e1b464e6de826dcc595db4
flextian <atian9@gatech.edu>
2022-03-14 23:01:11 -0400

    set up initial file structure and data

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# https://firedanger.cr.usgs.gov/apps/staticmaps - Bulk downloader
+# https://firedanger.cr.usgs.gov/viewer/index.html
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
